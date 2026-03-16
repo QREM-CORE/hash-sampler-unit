c321b4e3fda5dba882711f5f60e4a9d5e1031351
Kiet Le <ktlbusiness11@gmail.com>
2026-03-16 19:17:17 -0400

    REPO_INIT: Start Repo and setup submodules

---
+++ rtl.f
@@ -0,0 +1,4 @@
+# --- Packages (Must be compiled first) ---
+rtl/poly_arith_pkg.sv
+
+rtl/hash_sampler_unit.sv
---
+++ rtl/hash_sampler_unit.sv
@@ -0,0 +1,17 @@
+/*
+ * Module Name: hash_sampler_unit.sv
+ * Author(s): Kiet Le
+ * Target: FIPS 203 (ML-KEM / Kyber) Hardware Accelerator
+ *
+ * Reference:
+ *
+ * Description:
+ */
+
+import hash_sample_pkg::*;
+
+module hash_sampler_unit (
+
+);
+
+endmodule
